a143337a37a8477640b01b2e2065584fba6f6104
Panos32 <panagiotisdimitropoulos23@gmail.com>
2023-01-09 20:40:27 +0200

    DO IT!

---
+++ springboot/src/main/java/com/example/springboot/QuoteController.java
@@ -0,0 +1,26 @@
+package com.example.springboot;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "v1/quote")
+public class QuoteController {
+
+
+    private final QuoteService quoteService;
+
+    public QuoteController(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
+
+    @GetMapping
+	public List<Quote> getQuote() {
+        return quoteService.getQuote();
+	}
+
+    
+}
