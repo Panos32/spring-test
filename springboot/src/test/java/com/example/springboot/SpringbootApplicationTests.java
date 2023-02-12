4195c78410667c2aa1df4272c58581545caf13f6
Panos32 <panagiotisdimitropoulos23@gmail.com>
2023-02-12 22:07:21 +0200

    testing

--- springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.springboot;
 
+import static org.assertj.core.api.Assertions.assertThat; 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		// given 
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		//when
+		int result = underTest.add(numberOne, numberTwo);
+
+		// then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+
+	}
+
+	class Calculator{
+		int add(int a, int b){
+		return a= b ;}
 	}
 
 }
